1549deec2b45cf157cbb2af7ef6eafb82ac81a1d
snowmerak <snowmerak@outlook.com>
2022-01-22 04:04:47 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/snowmerak/error-tree/etree"
 )
 
 func main() {
-	Timeout := etree.New(errors.New("timeout"), nil)
-	Badrequest := etree.New(errors.New("bad request"), nil)
-	Notfound := etree.New(errors.New("not found"), nil)
-	Facebook := etree.New(errors.New("facebook"), nil)
-	Google := etree.New(errors.New("google"), nil)
+	Timeout := etree.New("timeout", nil)
+	Badrequest := etree.New("bad request", nil)
+	Notfound := etree.New("not found", nil)
+	Facebook := etree.New("facebook", nil)
+	Google := etree.New("google", nil)
 
-	FacebookTimeout := etree.New(errors.New("facebook timeout"), Timeout, Facebook)
-	FacebookBadrequest := etree.New(errors.New("facebook bad request"), Badrequest, Facebook)
-	FacebookNotfound := etree.New(errors.New("facebook not found"), Notfound, Facebook)
-	GoogleTimeout := etree.New(errors.New("google timeout"), Timeout, Google)
-	GoogleBadrequest := etree.New(errors.New("google bad request"), Badrequest, Google)
-	GoogleNotfound := etree.New(errors.New("google not found"), Notfound, Google)
+	FacebookTimeout := etree.New("facebook timeout", Timeout, Facebook)
+	FacebookBadrequest := etree.New("facebook bad request", Badrequest, Facebook)
+	FacebookNotfound := etree.New("facebook not found", Notfound, Facebook)
+	GoogleTimeout := etree.New("google timeout", Timeout, Google)
+	GoogleBadrequest := etree.New("google bad request", Badrequest, Google)
+	GoogleNotfound := etree.New("google not found", Notfound, Google)
 
 	fmt.Println(
 		etree.Cover(FacebookTimeout, Timeout), " ",
